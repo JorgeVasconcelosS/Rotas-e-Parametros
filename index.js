b43f40e1e3f5e51df317eac06cdb3818887c2174
Jorge Vasconcelos <vasconcelos.vsouza@gmail.com>
2025-07-09 14:30:22 -0300

    delete dos arquivos corrompidos

---
+++ index.js
@@ -0,0 +1,27 @@
+﻿import http from "http";
+
+const port = 3000;
+
+const grades = [
+  {
+    studentName: "Jorge",
+    subject: "Chinês",
+    grade: "9",
+  },
+];
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (url == "/grades" && method == "GET") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(grades));
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Route not found." }));
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}!!`);
+});
