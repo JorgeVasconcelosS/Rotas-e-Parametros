e07ea664d1e7f3ac4139c0681af8369549101030
Jorge Vasconcelos <vasconcelos.vsouza@gmail.com>
2025-07-09 19:12:28 -0300

    Aula Rotas e Parametros do curso node.js basics concluida.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 ﻿import http from "http";
+import { v4 } from "uuid";
 
 const port = 3000;
 
@@ -12,14 +13,27 @@ const grades = [
 
 const server = http.createServer((req, res) => {
   const { method, url } = req;
+  let body = "";
 
-  if (url == "/grades" && method == "GET") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(grades));
-  } else {
-    res.writeHead(404, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: "Route not found." }));
-  }
+  req.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+
+  req.on("end", () => {
+    if (url == "/grades" && method == "GET") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(grades));
+    } else if (url == "/grades" && method == "POST") {
+      const { studentName, subject, grade } = JSON.parse(body);
+      const newGrade = { id: v4(), studentName, subject, grade };
+      grades.push(newGrade);
+      res.writeHead(201, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(newGrade));
+    } else {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Route not found." }));
+    }
+  });
 });
 
 server.listen(port, () => {
